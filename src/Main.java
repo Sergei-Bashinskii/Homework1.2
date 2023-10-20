1ed06170c37479bf7f35ec56e89beab18af1bee1
sergei <sergei13121390@gmail.com>
2023-10-20 21:44:22 +0300

    Task

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Задача 6");
+        var a = 12;
+        var b = 27;
+        var c = 44;
+        var d = 15;
+        var e = 9;
+        var result =  a * (b + (c - d * e));
+        result = - result;
+        System.out.println(result);
+    }
+}
