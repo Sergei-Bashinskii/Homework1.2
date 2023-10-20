3227b34298e43fbffed8db9c57cd491f0e31bf7b
sergei <sergei13121390@gmail.com>
2023-10-20 23:19:25 +0300

    Task 8

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Задача 7");
-        var a = 5;
-        var b = 7;
-        a = a + b;
-        b = a - b;
-        a = a - b;
+        System.out.println("Задача 8");
+        var a = 495;
+        var b = a % 100;
+        b = b / 10;
         System.out.println("a = " + a);
         System.out.println("b = " + b);
     }
