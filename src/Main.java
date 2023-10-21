29d897fd6a1121cb3724f3009a7a233cc25bd36c
sergei <sergei13121390@gmail.com>
2023-10-21 14:09:06 +0300

    Task 6 Task 7 Task 8

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,34 @@
 public class Main {
     public static void main(String[] args) {
+        task6();
+        task7();
+        task8();
+    }
+
+    public static void task6(){
+        System.out.println("Задача 6");
+        var a = 12;
+        var b = 27;
+        var c = 44;
+        var d = 15;
+        var e = 9;
+        var result =  a * (b + (c - d * e));
+        result = - result;
+        System.out.println(result);
+    }
+
+    public static void task7(){
+        System.out.println("Задача 7");
+        var a = 5;
+        var b = 7;
+        a = a + b;
+        b = a - b;
+        a = a - b;
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+    }
+
+    public static void task8(){
         System.out.println("Задача 8");
         var a = 495;
         var b = a % 100;
